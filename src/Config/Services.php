fe74edbb2bbf4dc5c2211c4287cc0fa31b0fe49e
MGatner <mgatner@icloud.com>
2020-11-12 16:15:20 +0000

    Apply static analysis

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -2,23 +2,24 @@
 
 use CodeIgniter\Config\BaseService;
 use CodeIgniter\Database\ConnectionInterface;
+use Tatter\Settings\Config\Settings as SettingsConfig;
+use Tatter\Settings\Settings;
 
 class Services extends BaseService
 {
-	public static function settings(BaseConfig $config = null, bool $getShared = true)
+	/**
+	 * @param SettingsConfig|null $config
+	 * @param bool $getShared
+	 */
+	public static function settings(SettingsConfig $config = null, bool $getShared = true)
 	{
 		if ($getShared)
 		{
 			return static::getSharedInstance('settings', $config);
 		}
 
-		// If no config was injected then load one
-		// Prioritizes app/Config if found
-		if (empty($config))
-		{
-			$config = config('Settings');
-		}
+		$config = $config ?? config('Settings');
 
-		return new \Tatter\Settings\Settings($config);
+		return new Settings($config);
 	}
 }
